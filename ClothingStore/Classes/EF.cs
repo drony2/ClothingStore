8c29465c4b7cf660c0eafec2938b80517498b7c3
User <User@PIMPAY89>
2023-02-02 10:56:58 +0300

    Добавьте файлы проекта.

---
+++ ClothingStore/Classes/EF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothingStore.DB;
+
+namespace ClothingStore.Classes
+{
+    internal class EF
+    {
+       public static Entities Context { get; } = new Entities();
+    }
+}
---
+++ ClothingStore/Pages/Registration.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClothingStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSigIn_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.NavigationClass.navFrame.Navigate(new Pages.SingIn());
+        }
+    }
+}
